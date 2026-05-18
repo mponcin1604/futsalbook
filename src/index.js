488755067a039d17e00a40d6462cf85af7216377
mponcin1604 <mponcin1604@gmail.com>
2026-05-18 15:54:41 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./FutsalBook_App";
+import App from "./FutsalBook_App.jsx";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
